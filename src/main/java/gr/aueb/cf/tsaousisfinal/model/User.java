66ab5af73534aefe5b515e4acf130d814766bb1e
nijutsa <ntjuju82@gmail.com>
2025-01-17 17:17:26 +0200

    user implementing UserDetails

--- src/main/java/gr/aueb/cf/tsaousisfinal/model/User.java
+++ src/main/java/gr/aueb/cf/tsaousisfinal/model/User.java
@@ -8,8 +8,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -17,7 +21,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @Table(name = "users")
-public class User extends AbstractEntity{
+public class User extends AbstractEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -57,4 +61,28 @@ public class User extends AbstractEntity{
     private Warden Warden;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
